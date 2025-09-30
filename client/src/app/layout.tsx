b3eda61fe01fa5b8f9bbad721c852456df186c20
anibalalpizar <anibal.alpizar14@gmail.com>
2025-09-29 23:44:39 -0600

    Refactor RootLayout component: integrate ThemeProvider and Suspense for improved theme handling and loading states

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,8 @@ import { Toaster } from "@/components/ui/sonner"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
+import { ThemeProvider } from "next-themes"
+import { Suspense } from "react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +26,13 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <Suspense fallback={null}>{children}</Suspense>
+        </ThemeProvider>
         <Toaster />
       </body>
     </html>
