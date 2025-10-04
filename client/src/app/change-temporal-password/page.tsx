f520fbe08f31b36e805f5416e25d02ee94eaf90e
anibalalpizar <anibal.alpizar14@gmail.com>
2025-10-03 23:12:57 -0600

    Refactor reset password page to implement multi-step process with verification and new password creation

--- client/src/app/change-temporal-password/page.tsx
+++ client/src/app/change-temporal-password/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+"use client"
 
-function page() {
+import CreateNewPassword from "@/components/reset-password/CreateNewPassword"
+import PasswordResetSuccess from "@/components/reset-password/PasswordResetSuccess"
+import VerifyResetCode from "@/components/reset-password/VerifyResetCode"
+import { Card, CardContent } from "@/components/ui/card"
+import { useState } from "react"
+
+function ResetPasswordPage() {
+  const [step, setStep] = useState(1)
+  const [resetCode, setResetCode] = useState("")
+  const [newPassword, setNewPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+
   return (
-	<div>page</div>
+    <div className="min-h-screen  flex items-center justify-center p-4">
+      <Card className="w-full max-w-md ">
+        <CardContent className="pt-6">
+          {step === 1 && (
+            <VerifyResetCode
+              onNext={() => setStep(2)}
+              resetCode={resetCode}
+              setResetCode={setResetCode}
+            />
+          )}
+          
+          {step === 2 && (
+            <CreateNewPassword
+              onNext={() => setStep(3)}
+              newPassword={newPassword}
+              setNewPassword={setNewPassword}
+              confirmPassword={confirmPassword}
+              setConfirmPassword={setConfirmPassword}
+              temporalPassword={resetCode}
+            />
+          )}
+          
+          {step === 3 && <PasswordResetSuccess />}
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 
-export default page
+export default ResetPasswordPage
